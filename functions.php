752a810fe590d20c46356c808e8083e609dc0492
Auti <118858934+aolijar@users.noreply.github.com>
2023-05-31 15:40:12 -0500

    style cleanups

--- functions.php
+++ functions.php
@@ -29,7 +29,7 @@ function aeriz_setup() {
 	load_theme_textdomain( 'aeriz', get_template_directory() . '/languages' );
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	// add_theme_support( 'automatic-feed-links' );
 
 	/*
 		* Let WordPress manage the document title.
@@ -44,7 +44,7 @@ function aeriz_setup() {
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	// add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
@@ -83,22 +83,22 @@ function aeriz_setup() {
 	// );
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	// add_theme_support( 'customize-selective-refresh-widgets' );
 
 	/**
 	 * Add support for core custom logo.
 	 *
 	 * @link https://codex.wordpress.org/Theme_Logo
 	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
+	// add_theme_support(
+	// 	'custom-logo',
+	// 	array(
+	// 		'height'      => 250,
+	// 		'width'       => 250,
+	// 		'flex-width'  => true,
+	// 		'flex-height' => true,
+	// 	)
+	// );
 }
 add_action( 'after_setup_theme', 'aeriz_setup' );
 
@@ -119,20 +119,20 @@ add_action( 'after_setup_theme', 'aeriz_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function aeriz_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'aeriz' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'aeriz' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'aeriz_widgets_init' );
+// function aeriz_widgets_init() {
+// 	register_sidebar(
+// 		array(
+// 			'name'          => esc_html__( 'Sidebar', 'aeriz' ),
+// 			'id'            => 'sidebar-1',
+// 			'description'   => esc_html__( 'Add widgets here.', 'aeriz' ),
+// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 			'after_widget'  => '</section>',
+// 			'before_title'  => '<h2 class="widget-title">',
+// 			'after_title'   => '</h2>',
+// 		)
+// 	);
+// }
+// add_action( 'widgets_init', 'aeriz_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
@@ -152,9 +152,9 @@ function aeriz_scripts() {
 	wp_enqueue_script( 'frame-scrubber', get_template_directory_uri() . '/js/canvas-frame-scrubber.js', array(), $dev, true );
 	wp_enqueue_script( 'main-scroll', get_template_directory_uri() . '/js/main.js', array(), $dev, true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	// if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+	// 	wp_enqueue_script( 'comment-reply' );
+	// }
 }
 add_action( 'wp_enqueue_scripts', 'aeriz_scripts' );
 
